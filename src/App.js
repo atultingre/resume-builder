b1d15da943450ca5ab561aa10975940d9c35fe10
ATUL TINGRE <atultingre.work@gmail.com>
2023-05-22 14:55:29 +0530

    Body section with functionality completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import Body from './components/Body/Body';
-import Header from './components/Header/Header';
-
+import "./App.css";
+import Body from "./components/Body/Body";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </div>
   );
 }
